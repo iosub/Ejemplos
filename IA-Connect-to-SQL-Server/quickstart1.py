54a846927a15549ed348faca6e69af9fe5fe90de
iosub <1330995+iosub@users.noreply.github.com>
2024-05-16 11:06:37 +0200

    new file:   IA-Connect-to-SQL-Server/EjemPrompt.py 	modified:   IA-Connect-to-SQL-Server/quickstart1.py

--- IA-Connect-to-SQL-Server/quickstart1.py
+++ IA-Connect-to-SQL-Server/quickstart1.py
@@ -7,7 +7,21 @@ from langchain_community.utilities import SQLDatabase
 from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from langchain.chains import create_sql_query_chain
 
-#https://github.com/langchain-ai/langchain/blob/master/cookbook/sql_db_qa.mdx
+
+# Conectar a SQL Server
+conn_str = "mssql+pyodbc://gg:ostia@lenovo12/MspLitePro_V3GG?driver=ODBC+Driver+17+for+SQL+Server"
+db = SQLDatabase.from_uri(conn_str)
+llm = Ollama(model="llama3", temperature=0)
+chain = create_sql_query_chain(llm, db)
+response = chain.invoke({"question": "How many employees are there"})
+print(response)
+db.run(response)
+
+#https://python.langchain.com/v0.1/docs/use_cases/sql/
+#NOOOOOOOOOOOOOOOO https://github.com/langchain-ai/langchain/blob/master/cookbook/sql_db_qa.mdx
+#https://python.langchain.com/v0.1/docs/use_cases/sql/quickstart/
+#https://github.com/langchain-ai/langchain/issues/10325
+#https://python.langchain.com/v0.1/docs/use_cases/sql/query_checking/
 
 answer_prompt = PromptTemplate.from_template(
     """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
@@ -21,6 +35,6 @@ Answer: """
 conn_str = "mssql+pyodbc://gg:ostia@lenovo12/MspLitePro_V3GG?driver=ODBC+Driver+17+for+SQL+Server"
 db = SQLDatabase.from_uri(conn_str)
 llm = Ollama(model="llama3", temperature=0)
-sql_query_chain = create_sql_query_chain(llm, db, verbose=True)
+sql_query_chain = create_sql_query_chain(llm, db)
 
 sql_query_chain.run("How many employees are there?")
