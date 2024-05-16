0219d3f491513b30894b9aa2addde959589fefdd
iosub <1330995+iosub@users.noreply.github.com>
2024-05-16 17:23:59 +0200

    uyu

--- IA-Connect-to-SQL-Server/EjemploPromBeta.py
+++ IA-Connect-to-SQL-Server/EjemploPromBeta.py
@@ -11,18 +11,12 @@ from langchain_core.prompts import ChatPromptTemplate
 conn_str = "mssql+pyodbc://gg:ostia@lenovo12/MspLitePro_V3GG?driver=ODBC+Driver+17+for+SQL+Server"
 db = SQLDatabase.from_uri(conn_str)
 llm = Ollama(model="llama3", temperature=0)
-#=========================================================================================================================================================
+#===============================
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+chain = create_sql_query_chain(llm, db)
 
-system = """You are a {dialect} expert. Given an input question, creat a syntactically correct {dialect} query to run.
-Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per {dialect}. You can order the results to return the most informative data in the database.
-Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
-Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
-Pay attention to use date('now') function to get the current date, if the question involves "today".
-
-Only use the following tables:
-{table_info}
-
-Write an initial draft of the query. Then double check the {dialect} query for common mistakes, including:
+system = """Double check the user's {dialect} query for common mistakes, including:
 - Using NOT IN with NULL values
 - Using UNION when UNION ALL should have been used
 - Using BETWEEN for exclusive ranges
@@ -31,43 +25,25 @@ Write an initial draft of the query. Then double check the {dialect} query for c
 - Using the correct number of arguments for functions
 - Casting to the correct data type
 - Using the proper columns for joins
-- Using the proper columns names
-- Using the proper tables names
 
-Use format:
+If there are any of the above mistakes, rewrite the query. If there are no mistakes, just reproduce the original query.
 
-First draft: <<FIRST_DRAFT_QUERY>>
-Final answer: <<FINAL_ANSWER_QUERY>>
-"""
+Output the final SQL query only."""
 prompt = ChatPromptTemplate.from_messages(
-    [("system", system), ("human", "{input}")]
+    [("system", system), ("human", "{query}")]
 ).partial(dialect=db.dialect)
+validation_chain = prompt | llm | StrOutputParser()
 
-
-#def parse_final_answer(output: str) -> str:
-#    pp=(output.split("Final answer:")[1]).replace('```','')
-#    return (pp)
-
-def parse_final_answer(output: str) -> str:
-    pp=output.split("Final answer: ")[0]
-    return (pp)
-
-
-
-chain = create_sql_query_chain(llm, db, prompt=prompt) | parse_final_answer
-prompt.pretty_print()
-query = chain.invoke(
+full_chain = {"query": chain} | validation_chain
+query = full_chain.invoke(
     {
-        "question": "listame las facturas del cliente con denomi='GRUPO MZ'"
+        "question": "listame las facturas del cliente 'GRUPO MZ'"
     }
 )
 query
-
-
-print(query)
-print("========================================================================================")
-
+query=(query.split("```")[1]).replace('sql','')
 print(db.run(query))
+
 
 #write_query = create_sql_query_chain(llm, db)
 #print(write_query)
@@ -103,12 +79,19 @@ print(db.run(query))
 #FIN
 
 
-#===============================
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-chain = create_sql_query_chain(llm, db)
 
-system = """Double check the user's {dialect} query for common mistakes, including:
+#=========================================================================================================================================================
+
+system = """You are a {dialect} expert. Given an input question, creat a syntactically correct {dialect} query to run.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per {dialect}. You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use date('now') function to get the current date, if the question involves "today".
+
+Only use the following tables:
+{table_info}
+
+Write an initial draft of the query. Then double check the {dialect} query for common mistakes, including:
 - Using NOT IN with NULL values
 - Using UNION when UNION ALL should have been used
 - Using BETWEEN for exclusive ranges
@@ -117,21 +100,43 @@ system = """Double check the user's {dialect} query for common mistakes, includi
 - Using the correct number of arguments for functions
 - Casting to the correct data type
 - Using the proper columns for joins
+- Using the proper columns names
+- Using the proper table names
 
-If there are any of the above mistakes, rewrite the query. If there are no mistakes, just reproduce the original query.
 
-Output the final SQL query only."""
+Use format:
+
+First draft: <<FIRST_DRAFT_QUERY>>
+Final answer: <<FINAL_ANSWER_QUERY>>
+"""
 prompt = ChatPromptTemplate.from_messages(
-    [("system", system), ("human", "{query}")]
+    [("system", system), ("human", "{input}")]
 ).partial(dialect=db.dialect)
-validation_chain = prompt | llm | StrOutputParser()
 
-full_chain = {"query": chain} | validation_chain
-query = full_chain.invoke(
+
+def parse_final_answer(output: str) -> str:
+    pp=(output.split("Final answer:")[1]).replace('```','')
+    return (pp)
+
+#def parse_final_answer(output: str) -> str:
+#    pp=output.split("Final answer: ")[0]
+#    return (pp)
+
+
+
+chain = create_sql_query_chain(llm, db, prompt=prompt) | parse_final_answer
+prompt.pretty_print()
+query = chain.invoke(
     {
-        "question": "listame las facturas del cliente con denomi=GRUPO MZ"
+        #"question": "listame las facturas del cliente con denomi='GRUPO MZ'"
+         "question": "listame las facturas del cliente 'GRUPO MZ'"
     }
 )
 query
+
+query=(query.split("```")[1]).replace('sql','')
+print(query)
+print("========================================================================================")
+
 print(db.run(query))
 
